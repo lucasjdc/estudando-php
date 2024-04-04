38fbf9150b0311abb2d4f21a71f04ce106c5c406
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-04 19:20:32 -0300

    Add files via upload

--- conta.php
+++ conta.php
@@ -1,32 +1,44 @@
 <?php
 $titular = "José Fulano";
-$saldo = 1000.00;
-do {
-    
-    echo "********************\n";
-    echo "Titular: $titular\n";
-    echo "Saldo atual: R$ $saldo\n";
-    echo "********************\n";
+$saldo = 1_000;
+echo "********************\n";
+echo "Titular: $titular\n";
+echo "Saldo atual: R$ $saldo\n";
+echo "********************\n";
+do {    
     echo "1. Consultar saldo atual\n";
     echo "2. Sacar valor\n";
     echo "3. Depositar valor\n";
     echo "4. Sair\n";
     $comandoDoMenu = (int) fgets(STDIN);
-    if ($comandoDoMenu == 1) {
-        echo "Saldo: $saldo\n";
-    } else if ($comandoDoMenu == 2) {
-        $saque = (float) fgets(STDIN);
-        if ($saque <= $saldo) {
+    switch ($comandoDoMenu) {
+        case 1:
+            echo "*****************\n";
+            echo "Saldo atual: $saldo\n";
+            echo "*****************\n";
+            break;
+        case 2:
+            echo "Qual valor deseja sacar?\n";
+            $saque = (float) fgets(STDIN);
+            if ($saque <= $saldo) {
             $saldo = $saldo - $saque;
             echo "Saldo: $saldo\n";
-        } else if ($saque > $saldo) {
+            } else if ($saque > $saldo) {
             echo "Saldo insuficiente\n";
-        }
-    } else if ($comandoDoMenu == 3) {
-        $deposito = (float) fgets(STDIN);
-        $saldo = $saldo + $deposito;
+            }
+            break;
+        case 3:
+            echo "Qual valor deseja depositar?\n";
+            $deposito = (float) fgets(STDIN);
+            $saldo = $saldo + $deposito;
+            break;
+        case 4:
+            echo "Encerrando o programa.\n";
+            break;
+        default:
+            echo "Opção inválida!\n";
+            break;
     }
  } while ($comandoDoMenu != 4);
 
 ?>
-
